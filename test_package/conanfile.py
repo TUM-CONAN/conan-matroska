a2a23c8717927cccd9b259bdded92583d8a25f9c
Ulrich Eck <ulrich.eck@tum.de>
2023-04-25 18:06:02 +0200

    new conan2 package: matroska

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,13 +2,33 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.build import can_run
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+        self.requires("ebml/1.4.4@camposs/stable")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.set_property("ebml", "cmake_find_mode", "config")
+        deps.set_property("ebml", "cmake_file_name", "ebml")
+        deps.set_property("ebml", "cmake_target_name", "EBML::ebml")
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -16,5 +36,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        bin_path = os.path.join("bin", "test_package")
-        self.run(bin_path, run_environment=True)
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(cmd, env="conanrun")
